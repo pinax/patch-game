f4b30e0c36150c30c0ee49cb8f3bb7085bf1d973
Patrick Altman <paltman@gmail.com>
2018-02-01 20:50:49 -0600

    Fix path

--- patches/urls.py
+++ patches/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="homepage.html"), name="home"),
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
-    path("account/social/accounts/$", TemplateView.as_view(template_name="account/social.html"), name="account_social_accounts"),
+    path("account/social/accounts/", TemplateView.as_view(template_name="account/social.html"), name="account_social_accounts"),
     path("account/social/", include("social_django.urls", namespace="social")),
 ]
 
